5c49e0712f7aa8afcd7fa574449e9cbee51f170d
deep17raj <deeprajkumarwrs@gmail.com>
2025-12-24 22:39:46 +0530

    added fear

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://api.satotra.com/", // BACKEND
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
